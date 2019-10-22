ddf7ce03eed015ffb006f73d487ade3cac14e702
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2019-10-22 21:37:20 +0200

    Uzytownicy wiza siebie

--- ZPS/GameNew.py
+++ ZPS/GameNew.py
@@ -58,15 +58,15 @@ class GameServer(WebSocket):
 
       def handleConnected(self):
          print(self.address, 'connected')
-         for client in clients:
-            client.sendMessage(self.address[0] + u' - connected')
+         #for client in clients:
+         #   client.sendMessage(self.address[0] + u' - connected')
          clients.append(self)
 
       def handleClose(self):
          clients.remove(self)
          print(self.address, 'closed')
-         for client in clients:
-            client.sendMessage(self.address[0] + u' - disconnected')
+         #for client in clients:
+         #   client.sendMessage(self.address[0] + u' - disconnected')
 
 server = SimpleWebSocketServer('', 8000, GameServer)
 server.serveforever()
